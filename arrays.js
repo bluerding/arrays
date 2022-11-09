5e1d91186b250e4e88394882339a551a647c5451
Ben Luerding <bluerding@gmail.com>
2022-11-09 16:45:57 -0600

    add methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,15 @@
 var names = ["Ben", "Whitney", "Maya", "Bailey", "Anna"]
 var ages = ["30", "29", "6", "4", "2"]
-var boo = ["deposit paid", "have money", "am stressed", "im doing this correctly"
+var boo = ["deposit paid", "have money", "am stressed", "im doing this correctly"]
+
+//This will show element 1 from the names array, Whitney
+console.log(names[1])
+
+//This will remove the last element from the array
+ages.pop()
+
+console.log(ages)
+
+boo.shift()
+
+console.log(boo)
