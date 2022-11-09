4f17141f44aa0c87fa673e1351189a0fd841b762
Ben Luerding <bluerding@gmail.com>
2022-11-09 16:52:26 -0600

    add arrays

--- arrays.js
+++ arrays.js
@@ -5,11 +5,12 @@ var boo = ["deposit paid", "have money", "am stressed", "im doing this correctly
 //This will show element 1 from the names array, Whitney
 console.log(names[1])
 
-//This will remove the last element from the array
+//This will remove the last element from the ages array
 ages.pop()
 
 console.log(ages)
 
+//This will remove the first element from the boo array
 boo.shift()
 
 console.log(boo)
